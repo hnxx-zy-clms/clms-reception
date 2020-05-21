6e39d2b439b4b47a402eb1637c2a7c3eae8cac30
peng44 <1570505749@qq.com>
2020-05-21 23:51:33 +0800

    peng44 前台开发 --- 基本完成了报告批阅功能

--- src/views/report/marking/index.vue
+++ src/views/report/marking/index.vue
@@ -3,15 +3,22 @@
     <a-layout id="content" :style="{margin:'10px 35px 35px 35px',display:'flex'}">
       <!--      头部-->
       <a-layout-content :style="{height:'48px',background: '#F0F2F5', width: '100%' }">
-        <p :style="{float:'left',fontSize:'20px',marginRight:'10px'}">报告批阅</p>
+        <p v-if="roles === 1" :style="{float:'left',fontSize:'20px',marginRight:'10px'}">组长报告批阅</p>
+        <p v-else :style="{float:'left',fontSize:'20px',marginRight:'10px'}">班长报告批阅</p>
         <div :style="{marginTop:'5px'}">
-          <a-tag color="blue">
-            日报提交中
+          <a-tag v-if="day < 22" color="blue">
+            今日日报提交中
           </a-tag>
-          <a-tag color="blue">
-            周报提交中
+          <a-tag v-else color="red">
+            今日日报提交截止
           </a-tag>
-          <a-button :style="{ float: 'right',fontSize:'15px',marginBottom:'15px'}" type="link">
+          <a-tag v-if="week !== 6 && day < 22" color="blue">
+            本周周报提交中
+          </a-tag>
+          <a-tag v-else color="red">
+            本周周报提交截止
+          </a-tag>
+          <a-button :style="{ float: 'right',fontSize:'15px',marginBottom:'15px'}" type="link" @click="goback">
             返回
           </a-button>
         </div>
@@ -19,41 +26,68 @@
       <!--      中间Tips-->
       <a-layout-content :style="{ background: '#fff', marginTop: '5px'}">
         <a-menu v-model="current" mode="horizontal">
-          <a-menu-item key="login" @click="login=true">待批报告</a-menu-item>
-          <a-menu-item key="register" @click="login=false">已批报告</a-menu-item>
+          <a-menu-item key="login" @click="notMarkingInfo">待批报告</a-menu-item>
+          <a-menu-item key="register" @click="markingInfo">已批报告</a-menu-item>
         </a-menu>
-        <p style="margin: 10px 10px 10px 10px;color: #8c939d">已提交：1110 未提交：2000 剩余提交时间：12：20：01</p>
+        <p style="margin: 10px 10px 10px 10px;color: #8c939d">截止时间前，当天报告暂不显示
+          <a v-if="roles === 2" style="margin: 10px 10px 10px 10px;color: #8c939d">组长未批阅提交报告暂不显示</a></p>
       </a-layout-content>
       <!--      中间报告内容-->
       <a-layout-content :style="{ background: '#fff', marginTop: '5px' }">
-        <a-radio-group default-value="false" :style="{ background: '#fff', margin: '15px 15px 15px 15px' }">
-          <a-radio-button value="false">
-            日报
-          </a-radio-button>
-          <a-radio-button value="true">
-            周报
-          </a-radio-button>
-        </a-radio-group>
-        <div v-for="i in 5" v-bind:key="i">
-          <a-collapse :style="{ background: '#fff', margin: '15px 15px 15px 15px' }" accordion>
-            <a-collapse-panel key="1" header="2020-05-17 张三">
-              <p :style="{ float:'left'}">{{ text }}</p>
-              <div>
-                <a-icon type="form" :style="{float:'right',fontSize: '16px', color: '#08c' }" @click="showModal" />
-                <a-modal
-                  title="批阅"
-                  :visible="visible"
-                  :confirm-loading="confirmLoading"
-                  @ok="handleOk"
-                  @cancel="handleCancel"
-                >
-                  <a-rate v-model="value" count="10" />
-                  <p :style="{marginTop:'5px' }">{{ ModalText }}</p>
-                  <a-textarea placeholder="请输入内容" :rows="4" />
-                </a-modal>
-              </div>
-            </a-collapse-panel>
-          </a-collapse>
+        <div :style="{ background: '#fff', margin: '15px 15px 15px 15px' }">
+          <!--    报告类型按钮组-->
+          <a-radio-group
+            class="editable-add-btn"
+            :style="{ background: '#fff', marginBottom: '15px' }"
+            default-value="0"
+            @change="handleFormLayoutChange"
+          >
+            <a-radio-button id="dailyB" value="0">
+              日报
+            </a-radio-button>
+            <a-radio-button value="1">
+              周报
+            </a-radio-button>
+          </a-radio-group>
+          <!--    显示报告表格-->
+          <a-table
+            :pagination="false"
+            :style="{ marginRight:'15px' }"
+            :bordered="true"
+            :columns="columns"
+            :data-source="page.list"
+            :row-key="record => record.reportId"
+          >
+            <span slot="customTitle"><a-icon type="smile-o" /> 日期 </span>
+            <p slot="expandedRowRender" slot-scope="record" style="margin: 0">
+             报告ID： {{ record.reportId }}
+            </p>
+            <!--      操作报告-->
+            <span slot="action" slot-scope="scope">
+              <a-button v-if=" !markingState" type="primary" :style="{background:'#87d068',color:'#fff',marginBottom: '5px'}" @click="showModal(scope)">
+                批阅
+              </a-button>
+              <a-popover v-else placement="leftTop" title="批阅信息">
+                <template slot="content">
+                  <marking :report="scope.reportId" />
+                </template>
+                <a-button type="primary">
+                  批阅信息
+                </a-button>
+              </a-popover>
+            </span>
+          </a-table>
+          <!--    底部分页栏-->
+          <a-pagination
+            v-model="page.currentPage"
+            :style="{ marginTop:'20px',marginLeft:'45%' }"
+            :show-total="total => `共 ${total} 条`"
+            size="small"
+            :page-size="page.pageSize"
+            :total="page.totalCount"
+            show-quick-jumper
+            @change="pageChange"
+          />
         </div>
       </a-layout-content>
     </a-layout>
@@ -61,34 +95,147 @@
 </template>
 
 <script>
+import MarkingApi from '@/api/report/marking'
+import Marking from '../reportList/marking'
+import { mapGetters } from 'vuex'
+const columns = [
+  {
+    dataIndex: 'createdTime',
+    key: 'createdTime',
+    slots: { title: 'customTitle' },
+    width: '8%',
+    align: 'center',
+    scopedSlots: { customRender: 'createdTime' }
+  },
+  {
+    title: '姓名',
+    dataIndex: 'name',
+    width: '7%',
+    align: 'center',
+    key: 'name'
+  },
+  {
+    title: '组名',
+    dataIndex: 'userGroupId',
+    width: '6%',
+    align: 'center',
+    key: 'userGroupId'
+  },
+  {
+    title: '工作内容',
+    dataIndex: 'workContent',
+    width: '14%',
+    align: 'center',
+    key: 'workContent'
+  },
+  {
+    title: '遇到的困难',
+    width: '15%',
+    align: 'center',
+    dataIndex: 'difficulty',
+    key: 'difficulty'
+  },
+  {
+    title: '解决方法',
+    key: 'solution',
+    width: '15%',
+    align: 'center',
+    dataIndex: 'solution'
+  },
+  {
+    title: '心得体会',
+    key: 'experience',
+    width: '15%',
+    align: 'center',
+    dataIndex: 'experience'
+  },
+  {
+    title: '后续计划',
+    key: 'plan',
+    width: '15%',
+    align: 'center',
+    dataIndex: 'plan'
+  },
+  {
+    title: '操作',
+    key: 'action',
+    width: '5%',
+    scopedSlots: { customRender: 'action' }
+  }
+]
 export default {
   name: 'Index',
+  components: {
+    Marking
+  },
   data() {
     return {
-      login: false,
+      markingState: false,
       current: ['login'],
-      value: 3,
-      text: 'your name is my heart!',
-      ModalText: '评语：',
-      visible: false,
-      confirmLoading: false
+      columns,
+      week: new Date().getDay(),
+      day: new Date().getHours(),
+      page: {
+        currentPage: 1,
+        pageSize: 5,
+        totalCount: 0,
+        totalPage: 0,
+        params: { reportType: 0 },
+        sortColumn: 'created_time',
+        sortMethod: 'desc',
+        list: []
+      },
+      report: {}
     }
   },
+  computed: {
+    ...mapGetters([
+      'roles'
+    ])
+  },
+  created() {
+    this.getByPage()
+  },
   methods: {
-    showModal() {
-      this.visible = true
+    // 分页获取数据
+    getByPage() {
+      MarkingApi.getNotMarkingReport(this.page).then(res => {
+        this.page = res.data
+      })
     },
-    handleOk(e) {
-      this.ModalText = 'The modal will be closed after two seconds'
-      this.confirmLoading = true
-      setTimeout(() => {
-        this.visible = false
-        this.confirmLoading = false
-      }, 2000)
+    getMarkingByPage() {
+      MarkingApi.getMarkingReport(this.page).then(res => {
+        this.page = res.data
+      })
     },
-    handleCancel(e) {
-      console.log('Clicked cancel button')
-      this.visible = false
+    // 换报告类型
+    handleFormLayoutChange(e) {
+      this.page.params.reportType = e.target.value
+      if (this.markingState) {
+        this.getMarkingByPage()
+      } else {
+        this.getByPage()
+      }
+    },
+    // 换页
+    pageChange(pageNumber) {
+      this.page.currentPage = pageNumber
+      if (this.markingState) {
+        this.getMarkingByPage()
+      } else {
+        this.getByPage()
+      }
+    },
+    notMarkingInfo() {
+      this.markingState = false
+      this.getByPage()
+    },
+    markingInfo() {
+      this.markingState = true
+      this.getMarkingByPage()
+    },
+    goback() {
+      this.$router.go(-1)
     }
   }
 }
